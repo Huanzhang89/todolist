7ebb072a59bf5717367661a6a5e880566442554b
Huan Zhang <huanzhang30@gmail.com>
2014-06-25 13:42:42 +0100

    font awesome + objects

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-//Each function should only do one thing. And we also want to separate the data (model) from the html (view). This is done with like a manager (controller). This is a basic MVC separation that you'll have heard of (or will have heard of). It makes things easier for you to update things and change things as your data isn't mixed up with your html etc. 
-
-//So for instance here it's actually super difficult to delete your todo item, you have to delete the html and then you have to update the array
-
-//I'm going to refactor your JS so it's more MVC like, it doesn't have to be pure MVC, but just separate your functions so it reads better. Make sure your functions only do one thing and they are named accordingly. It's in maths. Shwoing you're working out
-
 var inputField = document.getElementById('todoText');
-var todoList = new Array();
+var todoList = [];
 var uList = document.getElementById('TodoTable');
 var addButton = document.getElementById('addTodo');
 var uListChild = document.getElementById('TodoTable').childNodes;
 var testButton = document.getElementById('testBtn');
+var todoObject = function (id, value) {
+	this.id = id;
+	this.value = value;
+};
+var todoListUid = [];
 
 
 
@@ -19,24 +18,24 @@ var addTodo = function (todoText) {
 };
 
 
-/*
-var deleteItem = function() {
-	if (uList.childNodes)	
-};
-*/
+
 
 var addToArray = function(todoText) {
-	var id;
-	for (var i=0; i<todoList.length; i++) {
-		id = i;
-		console.log(id);
-	}
-	var todoObject = {
-		id: id,
-		value: todoText
-	}
-	todoList.push(todoObject);
-}
+//use object constructor to create a new object evertime the button is clicked
+	if (todoList.length === 0) {
+		var todoObject1 = new todoObject (1, todoText);
+		todoList.push(todoObject1);
+		todoListUid = [todoObject1.id];
+		console.log(todoObject1.id);
+	} else {
+		var todoListLength = todoList.length + 1;
+		//for loop or if statement to create new todoObject and push it to the array
+		var todoObject2 = new todoObject (todoList.length+1, todoText);
+		todoList.push(todoObject2);
+		todoListUid.push(todoObject2.id);
+		console.log(todoList);
+	};
+};
 
 
 /*
@@ -49,7 +48,9 @@ inputField.addEventListener('click', function() {
 	inputField.value = "";
 });
 */
-
+var deleteItem = function (listItemId) {
+	todoList.splice(document.getElementById(listItemId).parentNode.id,1);
+};
 
 var refreshList = function() {
 	uList.innerHTML = "";
@@ -65,40 +66,23 @@ var refreshList = function() {
 		uList.appendChild(listItem);
 		listItem.appendChild(listItemCheckbox);
 		listItemCheckbox.addEventListener('click', function(){
-		todoList.splice(document.getElementById(listItemCheckbox.id).parentNode.id,1);
+		deleteItem(listItemCheckbox.id);
 		refreshList();
 		});
 		
 		
 	};
-}
+};
 
-/*
-testButton.addEventListener('click', function(a, b){
-		console.log(document.getElementById(checkboxId()));
+var insertPositionArrows = function () {
+	//loops through the array and add arrows
+	for (var i=0; i<todoList.length; i++) {
 		
-});
-*/
+	}
+}
 
 addButton.addEventListener('click', function(e){
 	var todoText = inputField.value;
 	addTodo(todoText);
 	inputField.value = "";
 });
-
-
-
-/*
-var cBoxChecker = function () {
-	for (i=0; i<todoList.length; i++) {
-	return todoList.checked;
-	return document.getElementsByName('cBox' + i).checked;
-	};
-};
-*/
-
-/*
-testButton.addEventListener('click', function(e) {
-	alert(cBoxCheck());
-});
-*/
